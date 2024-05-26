361b1b5a27538f61c51b63400a4bae71c3468703
aanxieee <153476178+aanxieee@users.noreply.github.com>
2024-05-27 02:09:51 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,10 @@
-function changeLanguage(lang){
-    
+  const languages = {
+    en: {
+        title: "Welcome to Prayagraj Boating",
+    },
+    hi: {
+        title: "प्रयागराज बोटिंग में आपका स्वागत है",
+        
+    }
+};
 }
